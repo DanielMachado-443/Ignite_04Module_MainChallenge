e57264f4b15e9ff6c110332e01a45b116cb6405f
DanielMachado-443 <srandantte@gmail.com>
2022-07-05 18:40:16 -0300

    First Attempt

---
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,39 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+
+
+describe("Create user", () => {
+  let inMemoryUsersRepository: InMemoryUsersRepository;
+  let createUserUseCase: CreateUserUseCase;
+
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository);
+  })
+
+  it("should be able to create a new user", async () => {
+    const user = await createUserUseCase.execute({
+      name: "test",
+      email: "test@test.com",
+      password: "1234"
+    })
+    expect(user).toHaveProperty("id")
+  })
+
+  it("should be able to create a new user with a email already registered", async () => {
+    await createUserUseCase.execute({
+      name: "test",
+      email: "test@test.com",
+      password: "1234"
+    })
+
+    await expect(
+      createUserUseCase.execute({
+        name: "test2",
+        email: "test@test.com",
+        password: "1234"
+      })
+    ).rejects.toEqual(new AppError("User already exists"))
+  })
+})
